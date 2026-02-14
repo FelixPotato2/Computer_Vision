4b2e0d3461e4d35b57c65db939f3cb1745831440
Marilia Merendi <mariliamerendi@gmail.com>
2026-02-14 17:40:44 +0100

    loop logic issue fixed

--- Assignment1.py
+++ Assignment1.py
@@ -5,7 +5,6 @@ import os
 import shutil
 
 pattern_size = (9,6)
-
 top_left = (0,0)
 top_right = (pattern_size[0] - 1, 0)
 bottom_left = (0, pattern_size[1] - 1)
@@ -54,7 +53,6 @@ def generate_board_points(cols, rows):
     board_points = (board_tl, board_tr, board_br, board_bl)
     return np.array(board_points, dtype = np.float32)
    
-
 def interpolate_corners(points, cols, rows):
     """
     Function that performs interpolation of image points.
@@ -77,65 +75,13 @@ def interpolate_corners(points, cols, rows):
     return img_grid
 
 
-#folders
-# bad_dir = "bad_images"
-# good_dir = "good_images"
-
-# # termination criteria
-# criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-# # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-# objp = np.zeros((6*9,3), np.float32)
-# objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
-
-# # Arrays to store object points and image points from all the images.
-# objpoints = [] # 3d point in real world space
-# imgpoints = [] # 2d points in image plane.
-
-# images = glob.glob('./images/*.jpg')
-
-# #clean up output directories
-# shutil.rmtree(good_dir, ignore_errors=True)
-# os.makedirs(good_dir)
-# shutil.rmtree(bad_dir, ignore_errors=True)
-# os.makedirs(bad_dir)
-
-# for fname in images:
-#     img = cv.imread(fname)
-#     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-#     # Find the chess board corners
-#     ret, corners = cv.findChessboardCorners(gray, (9,6), None)
-
-#     # If found, add object points, image points (after refining them)
-#     if ret == True:
-#         objpoints.append(objp)
-
-#         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-#         imgpoints.append(corners2)
-
-#         # Draw and display the corners
-#         cv.drawChessboardCorners(img, (9,6), corners2, ret)
-#         cv.imshow('img', img)
-#         cv.waitKey(500)
-
-#         print("Chessboard corners found in image: ", fname)
-#         out_path = os.path.join(good_dir, os.path.basename(fname))
-#         cv.imwrite(out_path, img)
-
-#     else :
-#         print("Chessboard corners not found in image: ", fname)
-#         out_path = os.path.join(bad_dir, os.path.basename(fname))
-#         cv.imwrite(out_path, img)
-
-#     cv.destroyAllWindows()
-
 manual_images = glob.glob('./bad_images/*.jpg')
+shutil.rmtree("new_images", ignore_errors=True)
+os.makedirs("new_images")
 
 
 for fname in manual_images:
-    shutil.rmtree("new_images", ignore_errors=True)
-    os.makedirs("new_images")
+    
     img = cv.imread(fname)
 
     state = {
